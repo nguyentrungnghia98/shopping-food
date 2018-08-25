6b7d272b36e5963f0628500433618c8a1a50baa5
ntn641998 <ntn641998@gmail.com>
2018-08-25 10:38:11 +0700

    a

--- src/js/models/Shopping.js
+++ src/js/models/Shopping.js
@@ -1,23 +1,22 @@
-export default class Shopping{
-    constructor(data){
-        this.data = data;
-    }
-    fixNumber  (){
-        // 4 1/2
-       
-        this.data.forEach(el=> {
-            // if (!el.number)
-             //{
-                el.number+= '';
-                const arr = el.number.split(' ');
-                let newNumber = parseFloat(arr[0],10) ;
-                if (arr.length === 2){
-                    newNumber+= eval(arr[1]);
-                }
-                el.number = newNumber;
-             //}
-         });
-        this.data.push(window.location.hash) ;
-        
-    }
+export default class Shopping {
+  constructor(data) {
+    this.data = data;
+  }
+  fixNumber() {
+    // 4 1/2
+
+    this.data.forEach(el => {
+      // if (!el.number)
+      //{
+      el.number += "";
+      const arr = el.number.split(" ");
+      let newNumber = parseFloat(arr[0], 10);
+      if (arr.length === 2) {
+        newNumber += eval(arr[1]);
+      }
+      el.number = newNumber;
+      //}
+    });
+    this.data.push(window.location.hash);
+  }
 }
